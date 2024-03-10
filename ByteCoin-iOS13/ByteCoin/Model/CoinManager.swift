d9dbefa2d3b3ba8590ec32616636514d9fb7a6c4
Haroldfromk <dongik369@naver.com>
2024-03-10 21:09:36 +0900

    updated

--- ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
+++ ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -11,7 +11,7 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "E87199E0-6ACC-46CB-9536-480902799542"
+   
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
