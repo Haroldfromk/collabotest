34bc3052e51360f8fa209212a83d96dba17c4df9
Haroldfromk <dongik369@naver.com>
2024-03-10 19:27:01 +0900

    made frame

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,22 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "E87199E0-6ACC-46CB-9536-480902799542"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+    func getCoinPrice(for : String){
+        
+    }
+}
---
+++ CollaboTest/CollaboTest/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CollaboTest
+//
+//  Created by Dongik Song on 3/9/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    var collabotest = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(collabotest)
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    
+}
+
