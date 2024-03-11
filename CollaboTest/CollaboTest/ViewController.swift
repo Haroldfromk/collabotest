fbff41f69682553b2d90a093c619b0ca5b0d817d
신승현 <sinseunghyeon313@gmail.com>
2024-03-11 11:18:25 +0900

    Update test

--- CollaboTest/CollaboTest/ViewController.swift
+++ CollaboTest/CollaboTest/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
         
         print(collabotest)
+        print("test")
         // Do any additional setup after loading the view.
     }
 
